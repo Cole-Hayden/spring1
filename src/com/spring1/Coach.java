a3c74d313280d11e81e34827cad8bf0487c2dcda
Cole-Hayden <cwh8b4@gmail.com>
2020-09-09 12:52:45 -0500

    spring injections

---
+++ src/com/spring1/Coach.java
@@ -0,0 +1,10 @@
+package com.spring1;
+
+public interface Coach {
+
+	public String getDailyWorkout();
+	
+	public String getDailyFortune();
+	
+	
+}
---
+++ src/com/spring1/FortuneService.java
@@ -0,0 +1,8 @@
+package com.spring1;
+
+public interface FortuneService {
+
+	
+	public String getFortune();
+	
+}
